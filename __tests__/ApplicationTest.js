2f354b0986c1175b014e206c8d9a931f63f03669
badahertz52 <badahertz52@gmail.com>
2023-10-25 14:09:34 +0900

    [test] Add new test

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -27,9 +27,9 @@ describe("숫자 야구 게임", () => {
   test("게임 종료 후 재시작", async () => {
     // given
     const randoms = [1, 3, 5, 5, 8, 9];
-    const answers = ["246", "135", "1", "597" ,"589", "2"];
+    const answers = ["246", "135", "1", "597","569" ,"598" ,"589", "2"];
     const logSpy = getLogSpy();
-    const messages = ["낫싱", "3스트라이크", "1볼 1스트라이크",, "3스트라이크", "게임 종료"];
+    const messages = ["낫싱", "3스트라이크","3개의 숫자를 모두 맞히셨습니다! 게임 종료","게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.", "1볼 1스트라이크","2스트라이크","2볼 1스트라이크", "3스트라이크", "게임 종료"];
 
     mockRandoms(randoms);
     mockQuestions(answers);
@@ -44,11 +44,24 @@ describe("숫자 야구 게임", () => {
     });
   });
 
-  test("예외 테스트", async () => {
+  test("예외 테스트:3자리 숫자", async () => {
     // given
     const randoms = [1, 3, 5];
-    const answers = ["1234"];
+    const answers = ["1234" ,"111", "12가" ,"012"];
 
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    // when & then
+    const app = new App();
+
+    await expect(app.play()).rejects.toThrow("[ERROR]");
+  });
+  test("예외 테스트:1 또는 2", async () => {
+    // given
+    const randoms = [1, 3, 5];
+    const answers = ["135","3"];
+    const messages = [ "3스트라이크","3개의 숫자를 모두 맞히셨습니다! 게임 종료","게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."];
     mockRandoms(randoms);
     mockQuestions(answers);
 
